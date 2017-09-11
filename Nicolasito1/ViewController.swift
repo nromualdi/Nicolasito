bbbfff5c16fe500c448329fe80414f3f81c94021
nromualdi <nicoromualdi@gmail.com>
2017-09-10 19:47:02 -0700

    Update

--- Nicolasito1/ViewController.swift
+++ Nicolasito1/ViewController.swift
@@ -9,8 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
-
-
+    @IBOutlet weak var Display: UILabel?
+    
+    var userIsInTheMiddleOfTyping:Bool = false
+    
+    @IBAction func touchbutton(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        let textcurrentlyindisplay = Display!.text!
+        if userIsInTheMiddleOfTyping {
+            Display!.text = textcurrentlyindisplay + digit
+        } else {
+            Display!.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+    }
+    
+    @IBAction func performoperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalsymbol = sender.currentTitle {
+            switch mathematicalsymbol {
+            case "π":
+                Display!.text = String(Double.pi)
+            default:
+                break
+            }}
+    }
+    
 }
-
