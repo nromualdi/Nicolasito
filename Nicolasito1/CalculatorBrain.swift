60bdee9d9f6902438b46015c8fd251c869e99f03
nromualdi <nicoromualdi@gmail.com>
2017-10-01 21:28:46 -0700

    Lecture 2 tbf

--- Nicolasito1/CalculatorBrain.swift
+++ Nicolasito1/CalculatorBrain.swift
@@ -8,22 +8,44 @@
 
 import Foundation
 
+func multiply(op1: Double, op2: Double) -> Double{
+    return op1 * op2
+}
+
+
 struct CalculatorBrain{
     
     private var accumulator: Double?
-    //I've made this an option because when I start the calculator the internal accumulator is not set, until I pass something into it.
+    //I've made this an optional because when I start the calculator the internal accumulator is not set, until I pass something into it.
+    
+    private enum Operation {//enums can take discrete values (case by case description of what it can be)
+        case constant(Double)
+        case unaryOperation((Double) -> (Double)) //unaryOperation is a function that takes a double and returns a double
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "π" : Operation.constant(Double.pi),
+        "e" : Operation.constant(M_E),
+        "√" : Operation.unaryOperation(sqrt),
+        "cos": Operation.unaryOperation(cos),
+        "x": Operation.binaryOperation(multiply)
+        "=": Operation.equals
+        ]
     
     mutating func performOperation(_ symbol: String){
-        switch symbol {
-        case "π":
-            accumulator = Double.pi
-        case "√":
-            if let operand = accumulator{
-                accumulator = sqrt(operand)}
-        default:
-            break
+        if let operation = operations[symbol] {
+            switch operation{
+            case .constant(let value):
+                accumulator = value
+            case .unaryOperation(let function):
+                if accumulator != nil{
+                    accumulator = function(accumulator!)
+              }
+            case .binaryOperation(let function)
+            }
         }
-        
     }
     
     mutating func setOperand(_ operand:Double)  {
